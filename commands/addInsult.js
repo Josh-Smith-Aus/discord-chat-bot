11bc984e143e3f5e6041655c8563291fea89bd53
Josh-Smith-Aus <joshuasmith_28@hotmail.com>
2020-09-25 23:27:50 +1000

    update

--- commands/addInsult.js
+++ commands/addInsult.js
@@ -1,7 +1,18 @@
 module.exports= {
-    name: 'addInsult',
+    name: 'addinsult',
     description: "add insult to insult list",
-	execute(message) {       
-        message.channel.send("hi there");
+	execute(message, args) {       
+     const {insults} = require("C:/Users/joshu/Documents/Discord chat bot/discord-chat-bot/insults.json");
+
+    //create small arrray then push that array to insults.json
+    
+//take args, put in new array, add spaces, push to insults
+        const newInsult = args.join(' ');//insult to string
+
+        insults.push(`${newInsult}`);
+
+    
+        message.channel.send(`Added "${newInsult}" to insult list`)
+
 	},
 };
